71ede731d36d8da00648ad4171989e5c052f0f9b
Seng Kuang Yap <syap0014@student.monash.edu>
2024-05-05 12:44:22 +1000

    fixed fornmat of assignments

--- studyplanner/src/app/components/assessments/page.tsx
+++ studyplanner/src/app/components/assessments/page.tsx
@@ -224,22 +224,65 @@ export default function Example() {
           <div className="text-center">
           <ul role="list" className="divide-y divide-gray-100">
             {assessments.map((assignment) => (
-              <li key={assignment.id} className="flex justify-between gap-x-6 py-5">
-                <div className="flex flex-col min-w-0 gap-y-1">
-                  <div className="min-w-0 flex-auto text-left">
-                    <p className="text-sm font-semibold leading-6 text-white">{assignment.name}</p>                    <p className="text-xs leading-6 text-white">{assignment.percentage} %</p>
-                    <p className="text-xs leading-6 text-white">Type of Assessment: {assignment.Type_of_Assessment}</p>
-                    <p className="text-xs leading-6 text-white">Topics: {assignment.topics}</p>
-                    <p className="text-xs leading-6 text-white">Description: {assignment.Description}</p> 
+              <li key={assignment.id} className="">
+                <div className='flex justify-between gap-x-6 py-5'>
+                  <div className="flex min-w-0 gap-x-4">
+                    <div className="min-w-0 flex-auto text-left">
+                      <p className="text-sm font-semibold leading-6 text-white">{assignment.name}</p>                    
+                      <p className="text-xs leading-6 text-white">{assignment.percentage} %</p>
+                      <p className="text-xs leading-6 text-white">Topics: {assignment.topics}</p>
+                      
+                    </div>
                   </div>
-                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                  <p className="text-sm leading-6 text-white">{assignment.dueDate}</p>
                   
+                  <div className="flex sm:hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                    <p className="text-sm leading-6 text-white">{assignment.dueDate}</p>
+                    <p className="text-xs leading-6 text-white">Type of Assessment: {assignment.Type_of_Assessment}</p>
+                    
+                  </div>
+                  
+                  <div className="hidden sm:flex sm:flex-col sm:items-end">
+                    <p className="text-sm leading-6 text-white">{assignment.dueDate}</p>
+                    <p className="text-xs leading-6 text-white">Type of Assessment: {assignment.Type_of_Assessment}</p>
+                  </div>
                 </div>
+                <div>
+                  <p className=" text-left text-xs leading-6 text-white">Description: {assignment.Description}</p>
                 </div>
+                
               </li>
             ))}
           </ul>
+
+
+          {/* <ul role="list" className="divide-y divide-gray-100">
+                {people.map((person) => (
+                  <li key={person.email} className="flex justify-between gap-x-6 py-5">
+                    <div className="flex min-w-0 gap-x-4">
+                      <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+                      <div className="min-w-0 flex-auto">
+                        <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
+                        <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                      </div>
+                    </div>
+                    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                      <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+                      {person.lastSeen ? (
+                        <p className="mt-1 text-xs leading-5 text-gray-500">
+                          Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                        </p>
+                      ) : (
+                        <div className="mt-1 flex items-center gap-x-1.5">
+                          <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                          </div>
+                          <p className="text-xs leading-5 text-gray-500">Online</p>
+                        </div>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul> */}
 
             
           </div>
